d14a0a9227a3757813677af5007e0e7f61e3a27c
Blaise Nduwimana <nduwimanablaise@gmail.com>
2026-06-04 13:18:42 +0200

    fix: increase sheduled work frequency from 2 min to 2 minutes

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 //Schedule::job(new SyncWialonUnitsJob)->everyTwoMinutes()->withoutOverlapping();
-Schedule::command('telemetry:sync')->everyTwoMinutes();
+Schedule::command('telemetry:sync')->everyMinute();
 Schedule::command('telemetry:archive')->dailyAt('02:00');
 Schedule::command('telemetry:maintain-partitions')->monthlyOn(1, '00:00');
