828f98cbdab91ed01873b6edcae805e4d16141b7
Blaise Nduwimana <nduwimanablaise@gmail.com>
2026-09-05 17:07:54 +0200

    feat: use Martin Logistics logo in admin portal sidebar

--- resources/js/portal/components/navigation/SidebarMenu.vue
+++ resources/js/portal/components/navigation/SidebarMenu.vue
@@ -1,8 +1,8 @@
 <template>
     <aside class="w-64 bg-white shadow-lg border-r border-gray-200 flex flex-col">
-        <div class="p-4 text-xl font-bold text-gray-700 flex items-center gap-2">
-            <svg class="w-6 h-6 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path></svg>
-            <span>Martin Logistics</span>
+        <div class="p-4 flex items-center gap-2">
+            <img src="/images/martin_hardware_logo.png" alt="Martin Logistics" class="h-10 w-auto">
+            <span class="text-xl font-bold text-gray-700">Martin Logistics</span>
         </div>
 
         <nav class="flex-1 overflow-y-auto px-2 py-4 space-y-2">
